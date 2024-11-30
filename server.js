86117aaef8d4839b429fb735a7f9228b3e427b0b
ccarletti10 <ccarletti10@gmail.com>
2024-11-30 16:19:55 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const app = express();
